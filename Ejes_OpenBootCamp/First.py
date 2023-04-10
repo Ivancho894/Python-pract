58ec60b459446738c964c328166e82db7905d8e5
Ivancho894 <serralta.ivan@gmail.com>
2023-04-10 16:04:24 -0300

    Several exercises

---
+++ Ejes_OpenBootCamp/First.py
@@ -0,0 +1,11 @@
+#Hello world program
+
+
+
+a = "Hola mundo"
+
+print(a)
+
+a = "Variable modificada"
+
+print(a)
---
+++ Ejes_OpenBootCamp/Fourth.py
@@ -0,0 +1,11 @@
+#Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
+
+
+
+def to100 ():
+    num = 100
+    while num!=0:
+        print (num)
+        num-=1
+
+to100()
