bf3fa490381ab7b51a94abb0edfa8701dd375e94
Ivancho894 <serralta.ivan@gmail.com>
2023-04-10 17:05:42 -0300

    Ex number 8

---
+++ Ejes_OpenBootCamp/eight/eigthV2.py
@@ -0,0 +1,6 @@
+from Ejes_OpenBootCamp.eight import eigth
+
+a = 8
+b = 4
+
+print("Numeros: ", a," y ", b,"Suma: ", eigth.sumar(a, b), "Resta: ", eigth.restar(a, b), "Division: ", eigth.dividir(a, b), "Multiplicacion: ", eigth.multiplicar(a, b))
